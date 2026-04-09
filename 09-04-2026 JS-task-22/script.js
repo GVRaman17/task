dfa585d2e48807dba78c5ebd4579d5142d0d21ac
VENKATRAMAMOORTHY <86295246+GVRaman17@users.noreply.github.com>
2026-04-09 16:57:28 +0530

    Add files via upload

---
+++ 09-04-2026 JS-task-22/script.js
@@ -0,0 +1,7 @@
+let obj={name:'alice',age:21,sayHello:function(){return "hello world"}};
+console.log(obj);
+console.log(obj.name);
+console.log(obj.age);
+obj.age=22;
+console.log(obj.age);
+console.log(obj.sayHello());
