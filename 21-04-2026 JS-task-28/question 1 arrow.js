10019739418499480e226a9bda575ff245b66129
VENKATRAMAMOORTHY <86295246+GVRaman17@users.noreply.github.com>
2026-04-21 16:54:45 +0530

    Add files via upload

---
+++ 21-04-2026 JS-task-28/question 1 arrow.js
@@ -0,0 +1,8 @@
+let square = x => x*x;
+let add =(a,b) => a+b;
+let greet =()=>'hello';
+let createuser = (name,age)=>({name,age});
+console.log(square(3));
+console.log(add(4,6));
+console.log(greet());
+console.log(createuser('venkat',22));
